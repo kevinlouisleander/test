c67524c21404d66392203468318a083eccca2798
kevinlouisleander <159959681+kevinlouisleander@users.noreply.github.com>
2024-02-23 14:14:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 # app.py
 from flask import Flask
+import os
 
 app = Flask(__name__)
+
+port = int(os.environ.get('PORT', 3000))
 
 @app.route('/')
 def index():
     return "Company: Wild Rydes\nDeveloper: Kevin Leander\nStudent ID: 100918906"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=port)
